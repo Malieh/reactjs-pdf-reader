255b66c2eb048f274051694e0361391cb2a79079
greatweb <malieh@prepdia.com>
2020-03-30 11:45:47 +0100

    compiled the lib

--- dist/app.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var React = require("react");
-var react_dom_1 = require("react-dom");
-var test_1 = require("./test");
-var rootEl = document.getElementById("root");
-react_dom_1.render(React.createElement(test_1.default, null), rootEl);
-//# sourceMappingURL=app.js.map
